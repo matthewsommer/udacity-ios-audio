e2f7c5d93b41c7dcbeb4374d0137a7a008a42dc8
Matt Sommer <matthew.sommer@gmail.com>
2015-07-22 09:54:34 -0700

    Removed commented code and unnecessary override.

--- Pitch Perfect/PlaySoundsViewController.swift
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -27,22 +27,6 @@ class PlaySoundsViewController: UIViewController {
         audioPlayer.enableRate = true;
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
     @IBAction func PlaySlow(sender: UIButton) {
         println("in PlaySlow")
         playAudioWithVariableRate(0.5)
